ae31d3ff50ccfe1d7e572e00259a4e99c5e51f45
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-11 12:59:59 +0530

    - displayed ids

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ while True:
 
     for classId, score, bbox in zip(classIds, scores, bboxes):
         (x, y, w, h) = bbox
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.putText(frame, str(classId), (x, y-10), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (200, 0, 50), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (200,0,50), 2)
 
     cv2.imshow('Live', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
