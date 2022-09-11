431aeea0bba2b17216d28a178df159e9f7224ab5
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-11 12:03:59 +0530

    - video feed running

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import cv2
 
+feed = cv2.VideoCapture(0)
+
+while True:
+    check, frame = feed.read()
+    flipFeed = cv2.flip(frame, 1)
+    cv2.imshow('Live', flipFeed)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
